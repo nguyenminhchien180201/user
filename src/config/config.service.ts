94d3b3715a29ec50c31c254718d7944477d451c4
nguyenminhchien180201 <111742403+nguyenminhchien180201@users.noreply.github.com>
2023-07-25 17:18:09 +0700

    done query db

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -6,28 +6,28 @@ import * as SharedEntities from 'shared-entities';
 
 @Injectable()
 export class ConfigService {
-    postgresTypeormConfigAloline(): Promise<TypeOrmModuleOptions> | TypeOrmModuleOptions {
-        return {
-            type: 'postgres',
-            host: process.env.CONFIG_POSTGRESQL_HOST_ALOLINE,
-            port: parseInt(process.env.CONFIG_POSTGRESQL_PORT_ALOLINE),
-            username: process.env.CONFIG_POSTGRESQL_USER_NAME_ALOLINE,
-            password: process.env.CONFIG_POSTGRESQL_PASSWORD_ALOLINE,
-            database: process.env.CONFIG_POSTGRESQL_KEYSPACE_ALOLINE,
-            entities: [
-            ],
-        };
-    }
+    // postgresTypeormConfigAloline(): Promise<TypeOrmModuleOptions> | TypeOrmModuleOptions {
+    //     return {
+    //         type: 'postgres',
+    //         host: process.env.CONFIG_POSTGRESQL_HOST_ALOLINE,
+    //         port: parseInt(process.env.CONFIG_POSTGRESQL_PORT_ALOLINE),
+    //         username: process.env.CONFIG_POSTGRESQL_USER_NAME_ALOLINE,
+    //         password: process.env.CONFIG_POSTGRESQL_PASSWORD_ALOLINE,
+    //         database: process.env.CONFIG_POSTGRESQL_KEYSPACE_ALOLINE,
+    //         entities: [
+    //         ],
+    //     };
+    // }
 
-    postgresTypeormConfigShared(): Promise<TypeOrmModuleOptions> | TypeOrmModuleOptions {
-        return {
-            type: 'postgres',
-            host: process.env.CONFIG_POSTGRESQL_HOST_SHARED,
-            port: parseInt(process.env.CONFIG_POSTGRESQL_PORT_SHARED),
-            username: process.env.CONFIG_POSTGRESQL_USER_NAME_SHARED,
-            password: process.env.CONFIG_POSTGRESQL_PASSWORD_SHARED,
-            database: process.env.CONFIG_POSTGRESQL_KEYSPACE_SHARED,
-            entities: [SharedEntities.UserEntity],
-        };
-    }
+    // postgresTypeormConfigShared(): Promise<TypeOrmModuleOptions> | TypeOrmModuleOptions {
+    //     return {
+    //         type: 'postgres',
+    //         host: process.env.CONFIG_POSTGRESQL_HOST_SHARED,
+    //         port: parseInt(process.env.CONFIG_POSTGRESQL_PORT_SHARED),
+    //         username: process.env.CONFIG_POSTGRESQL_USER_NAME_SHARED,
+    //         password: process.env.CONFIG_POSTGRESQL_PASSWORD_SHARED,
+    //         database: process.env.CONFIG_POSTGRESQL_KEYSPACE_SHARED,
+    //         entities: [SharedEntities.UserEntity],
+    //     };
+    // }
 }
